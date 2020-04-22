df79e2a6d0313346965ca152de33dba4d0aa23f2
Yoshinori Osawa <yosawa@decurret.co.jp>
2020-04-22 13:57:19 +0900

    SwiftPM管理のLicense反映もれ対応

--- Scripts/update_license_plist.sh
+++ Scripts/update_license_plist.sh
@@ -3,9 +3,11 @@
 base_dir="TraceCovid19"
 output_path="${base_dir}/Resources/Settings.bundle"
 name="Acknowledgements"
+swiftpm="TraceCovid19.xcworkspace/xcshareddata/swiftpm/Package.swift"
 
 mint run LicensePlist\
   --output-path ${output_path}\
   --suppress-opening-directory\
   --prefix ${name}\
+  --package-path ${swiftpm}\
   --single-page
