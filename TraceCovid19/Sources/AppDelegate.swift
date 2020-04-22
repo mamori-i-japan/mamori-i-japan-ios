d7b8981cdaf9578c18d41622d7123e01c8a810c0
Masakazu Ohtsuka <o.masakazu@gmail.com>
2020-04-22 17:23:54 +0300

    debugNotify

--- TraceCovid19/Sources/AppDelegate.swift
+++ TraceCovid19/Sources/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        log("launchOptions=\(String(describing: launchOptions))")
         PrintUtility.shared.isHidden = true
         FirebaseApp.configure()
         return true
